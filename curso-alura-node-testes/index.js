9f6eaa166d835b816fefef93457a3e3d23f27e90
Larissa-LM <larissa.maria@creditas.com>
2022-10-20 14:35:13 -0300

    Primiero teste de unidade

--- curso-alura-node-testes/index.js
+++ curso-alura-node-testes/index.js
@@ -1,3 +1,14 @@
 const somaHoraExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHoraExtras(2000, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (nomeDaFuncao, ValorEsperado, ValorRetornado) => {
+  if (ValorEsperado === ValorRetornado) {
+    console.log(`${nomeDaFuncao} passou`);
+  } else {
+    console.error(`${nomeDaFuncao} não passou`);
+  }
+};
+
+teste('calculaDescontos', 2200, calculaDescontos(2500, 300));
+teste('somaHorasExtras', 2500, somaHoraExtras(2000, 500));
